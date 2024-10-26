cc77d5b8634d850f374fa80fb3604cc92017abd3
Olof Johnsson <olof.s.johnsson@gmail.com>
2024-10-26 20:01:35 +0200

    Add interrupt on both edges for sw_2

--- src/application/application.c
+++ src/application/application.c
@@ -23,11 +23,26 @@ static const struct gpio_dt_spec sw_pwr_pin_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(sw
 static const struct gpio_dt_spec sw_pwr_pin_2 = GPIO_DT_SPEC_GET(DT_NODELABEL(sw_pwr_2), gpios);
 
 static struct gpio_callback sw_1_cb_data;
+static struct gpio_callback sw_2_cb_data;
 
 /* Interrupt callback function */
 void sw_1_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
     if (gpio_pin_get_dt(&sw_pin_1))
+    {
+        gpio_pin_set_dt(&led, 0);
+    }
+    else
+    {
+        gpio_pin_set_dt(&led, 1);
+    }
+    
+}
+
+/* Interrupt callback function */
+void sw_2_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+    if (gpio_pin_get_dt(&sw_pin_2))
     {
         gpio_pin_set_dt(&led, 0);
     }
@@ -49,12 +64,19 @@ void init_pins()
     gpio_pin_configure_dt(&sw_pin_2, GPIO_INPUT);
     gpio_pin_configure_dt(&pir_pin_1, GPIO_INPUT);
 
-    /* Configure sw_pin_1 as interrupt on falling edge */
+    /* Configure sw_pin_1 as interrupt on both edges */
     gpio_pin_interrupt_configure_dt(&sw_pin_1, GPIO_INT_EDGE_BOTH);
 
     /* Initialize GPIO callback */
     gpio_init_callback(&sw_1_cb_data, sw_1_pressed, BIT(sw_pin_1.pin));
     gpio_add_callback(sw_pin_1.port, &sw_1_cb_data);
+
+    /* Configure sw_pin_1 as interrupt on both edges */
+    gpio_pin_interrupt_configure_dt(&sw_pin_2, GPIO_INT_EDGE_BOTH);
+
+    /* Initialize GPIO callback */
+    gpio_init_callback(&sw_2_cb_data, sw_1_pressed, BIT(sw_pin_2.pin));
+    gpio_add_callback(sw_pin_2.port, &sw_2_cb_data);
 
     printk("Pins initialized and interrupt configured for sw_pin_1\n");
 }
