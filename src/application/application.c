f016c1464dfcc2741db0630afb31246ffd125d65
Olof Johnsson <olof.s.johnsson@gmail.com>
2024-10-30 21:38:33 +0100

    Add PIR motion sensor

--- src/application/application.c
+++ src/application/application.c
@@ -8,9 +8,20 @@
 #include <hal/nrf_power.h>
 
 typedef enum {
-    CW,   // 0
-    CCW   // 1
-} direction;
+    CW,
+    CCW
+} motor_direction;
+
+typedef enum {
+    NO_MOTION_DETECTED,
+    MOTION_DETECTED
+} pir_detection;
+
+typedef enum {
+    MOTOR_NOT_RUNNING,
+    MOTOR_RUNNING,
+    MOTOR_REWIND
+} motor_activity;
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(redled), gpios);
 
@@ -21,24 +32,25 @@ static const struct gpio_dt_spec sw_pin_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(sw_1),
 static const struct gpio_dt_spec sw_pin_2 = GPIO_DT_SPEC_GET(DT_NODELABEL(sw_2), gpios);
 static const struct gpio_dt_spec pir_pin_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(pir_1), gpios);
 static const struct gpio_dt_spec button_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(button_1), gpios);
-//static const struct gpio_dt_spec sw_pwr_1 = GPIO_DT_SPEC_GET(DT_NODELABEL(sw_pwr_1), gpios);
 
+/* Declare callbacks */
 static struct gpio_callback sw_1_cb_data;
 static struct gpio_callback sw_2_cb_data;
 static struct gpio_callback pir_1_cb_data;
 
-static uint8_t dir = CW;
+static uint8_t pir_motion = NO_MOTION_DETECTED;
+static uint8_t motor = MOTOR_NOT_RUNNING;
 
 void run_zipline(int direction)
 {
+    motor = MOTOR_RUNNING;
     switch (direction)
     {
-        case 0:
+        case CW:
             gpio_pin_set_dt(&gate_pin_2, 1);
             break;
-        case 1:
+        case CCW:
             gpio_pin_set_dt(&gate_pin_1, 1);
-            
             break;
         default:
             gpio_pin_set_dt(&gate_pin_1, 0);
@@ -47,28 +59,28 @@ void run_zipline(int direction)
     }
 }
 
-/* Interrupt callback function */
+/* Interrupt callback function for zip line limit switch 1 */
 void sw_1_activated(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
     gpio_pin_set_dt(&gate_pin_1, 0);
     gpio_pin_set_dt(&gate_pin_2, 0);
     gpio_pin_set_dt(&led, 0);
-    dir = CCW;
+    motor = MOTOR_REWIND;
 }
 
-/* Interrupt callback function */
+/* Interrupt callback function for zip line limit switch 2 */
 void sw_2_activated(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
     gpio_pin_set_dt(&gate_pin_1, 0);
     gpio_pin_set_dt(&gate_pin_2, 0);
     gpio_pin_set_dt(&led, 1);
-    dir = CW;
+    motor = MOTOR_NOT_RUNNING;
 }
 
-// Shared callback function
+/* Interrupt callback function for PIR motion sensor */
 void pir_activated(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-    //gpio_pin_set_dt(&led, 0);
+    run_zipline(CW);
 }
 
 void init_pins()
@@ -77,8 +89,8 @@ void init_pins()
     gpio_pin_configure_dt(&gate_pin_1, GPIO_OUTPUT_INACTIVE);
     gpio_pin_configure_dt(&gate_pin_2, GPIO_OUTPUT_INACTIVE);
     //gpio_pin_configure_dt(&sw_pwr_1, GPIO_OUTPUT_ACTIVE);
-    gpio_pin_configure_dt(&sw_pin_1, GPIO_INPUT);
-    gpio_pin_configure_dt(&sw_pin_2, GPIO_INPUT);
+    gpio_pin_configure_dt(&sw_pin_1, GPIO_PULL_UP);
+    gpio_pin_configure_dt(&sw_pin_2, GPIO_PULL_UP);
     gpio_pin_configure_dt(&pir_pin_1, GPIO_PULL_DOWN);
     gpio_pin_configure_dt(&button_1, GPIO_PULL_UP);
 
@@ -114,10 +126,13 @@ void run_application()
 
     gpio_pin_set_dt(&led, 1);
 
-    run_zipline(CW);
-
     while (1) 
     {
+        if(motor == MOTOR_REWIND)
+        {
+            k_msleep(5000);s
+            run_zipline(CCW);
+        }
         k_msleep(1000);
     }
 }
